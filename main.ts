6a2c025904b18bf02371ee76977593b800a1d96a
Chamsseddine03 <132678444+Chamsseddine03@users.noreply.github.com>
2023-05-15 08:59:30 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
+})
+radio.setGroup(70)
+radio.setFrequencyBand(70)
+radio.setTransmitPower(7)
